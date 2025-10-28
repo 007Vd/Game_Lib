6fd3f73258d40f932e2725fd2b4eb975766656d4
007Vd <vdeogade007@gmail.com>
2025-10-28 10:38:09 +0530

    Add files via upload

--- project.sql
+++ project.sql
@@ -1,3 +1,5 @@
+use mydbs;
+
 CREATE TABLE Genres(
 genre_id INT PRIMARY KEY,
 genre_name VARCHAR(45) NOT NULL
@@ -32,8 +34,58 @@ title VARCHAR(100),
 platform VARCHAR(50),
 release_year YEAR,
 price DECIMAL(6,2),
-availability ENUM('Available','Unavailable'),
+availability ENUM('Available','unavailable'),
 avg_rating DECIMAL(2,1),
 genre_id INT,
 FOREIGN KEY(genre_id) REFERENCES Genres(genre_id)
 );
+show tables;
+CREATE TABLE Rentals (
+    rental_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    game_id INT,
+    rental_date DATE NOT NULL,
+    return_date DATE,
+    status ENUM('Rented','Returned','Overdue') DEFAULT 'Rented',
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (game_id) REFERENCES Games(game_id)
+);
+CREATE TABLE Reviews (
+    review_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    game_id INT,
+    rating DECIMAL(2,1) CHECK (rating BETWEEN 0 AND 5),
+    comment VARCHAR(255),
+    review_date DATE,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (game_id) REFERENCES Games(game_id)
+);
+CREATE TABLE Payments (
+    payment_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    amount DECIMAL(8,2) NOT NULL,
+    payment_date DATE NOT NULL,
+    payment_type ENUM('Membership','Rental'),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+CREATE TABLE Game_Developer (
+    game_id INT,
+    dev_id INT,
+    PRIMARY KEY (game_id, dev_id),
+    FOREIGN KEY (game_id) REFERENCES Games(game_id),
+    FOREIGN KEY (dev_id) REFERENCES Developers(dev_id)
+);
+CREATE TABLE Purchases (
+    purchase_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    game_id INT,
+    purchase_date DATE NOT NULL,
+    price DECIMAL(8,2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (game_id) REFERENCES Games(game_id)
+);
+
+show tables;
+
+
+
